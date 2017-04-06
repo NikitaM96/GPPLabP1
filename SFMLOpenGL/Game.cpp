e788562fa2d379c4bb6f5d13a0b17118caf3e5e8
NIKITA MULLINS <C00207976@itcarlow.ie>
2017-04-06 14:57:36 +0100

    finished

--- SFMLOpenGL/Game.cpp
+++ SFMLOpenGL/Game.cpp
@@ -13,7 +13,7 @@ string toString(T number)
 
 GLuint	vsid,		// Vertex Shader ID
 		fsid,		// Fragment Shader ID
-		progID,		// Program ID
+		enemyProgID, playerProgID,		// Program ID
 		vao = 0,	// Vertex Array ID
 		vbo,		// Vertex Buffer ID
 		vib,		// Vertex Index Buffer
@@ -87,21 +87,24 @@ void Game::run()
 
 			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
 			{
-				// Set Model Rotation
-				//player = rotate(player, 0.01f, glm::vec3(0, 1, 0)); // Rotate
-				player = translate(player, vec3(-0.1, 0, 0));
-				playerVec.x = playerVec.x - 0.1;
-				playerVec.z = player[3].z;
+				if (player[3].x > -3.9)
+				{
+
+					// Set Model Rotation
+					//player = rotate(player, 0.01f, glm::vec3(0, 1, 0)); // Rotate
+					player = translate(player, vec3(-0.5, 0, 0));
+				}
 				
 			}
 
 			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
 			{
-				// Set Model Rotation
-				//player = rotate(player, -0.01f, glm::vec3(0, 1, 0)); // Rotate
-				player = translate(player, vec3(0.1, 0, 0));
-				playerVec.x = playerVec.x + 0.1;
-				playerVec.z = player[3].z;
+				if (player[3].x < 3.9)
+				{
+					// Set Model Rotation
+					//player = rotate(player, -0.01f, glm::vec3(0, 1, 0)); // Rotate
+					player = translate(player, vec3(0.5, 0, 0));
+				}
 			}
 
 			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
@@ -123,15 +126,13 @@ void Game::run()
 #if (DEBUG >= 2)
 	DEBUG_MSG("Calling Cleanup...");
 #endif
-	unload();
-
+	unload(enemyProgID);
+	unload(playerProgID);
 }
 
 void Game::initialize()
 {
 	isRunning = true;
-	GLint isCompiled = 0;
-	GLint isLinked = 0;
 
 	if (!(!glewInit())) { DEBUG_MSG("glewInit() failed"); }
 
@@ -156,98 +157,10 @@ void Game::initialize()
 	// Indices to be drawn
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 3 * INDICES * sizeof(GLuint), indices, GL_STATIC_DRAW);
 
-	// NOTE: uniforms values must be used within Shader so that they 
-	// can be retreived
-	const char* vs_src =
-		"#version 400\n\r"
-		""
-		"in vec3 sv_position;"
-		"in vec4 sv_color;"
-		"in vec2 sv_uv;"
-		""
-		"out vec4 color;"
-		"out vec2 uv;"
-		""
-		"uniform mat4 sv_mvp;"
-		"uniform float sv_x_offset;"
-		"uniform float sv_y_offset;"
-		"uniform float sv_z_offset;"
-		""
-		"void main() {"
-		"	color = sv_color;"
-		"	uv = sv_uv;"
-		//"	gl_Position = vec4(sv_position, 1);"
-		"	gl_Position = sv_mvp * vec4(sv_position.x + sv_x_offset, sv_position.y + sv_y_offset, sv_position.z + sv_z_offset, 1 );"
-		"}"; //Vertex Shader Src
-
-	DEBUG_MSG("Setting Up Vertex Shader");
-
-	vsid = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vsid, 1, (const GLchar**)&vs_src, NULL);
-	glCompileShader(vsid);
-
-	// Check is Shader Compiled
-	glGetShaderiv(vsid, GL_COMPILE_STATUS, &isCompiled);
-
-	if (isCompiled == GL_TRUE) {
-		DEBUG_MSG("Vertex Shader Compiled");
-		isCompiled = GL_FALSE;
-	}
-	else
-	{
-		DEBUG_MSG("ERROR: Vertex Shader Compilation Error");
-	}
-
-	const char* fs_src =
-		"#version 400\n\r"
-		""
-		"uniform sampler2D f_texture;"
-		""
-		"in vec4 color;"
-		"in vec2 uv;"
-		""
-		"out vec4 fColor;"
-		""
-		"void main() {"
-		"	fColor = color + texture2D(f_texture, uv);"
-		""
-		"}"; //Fragment Shader Src
-
-	DEBUG_MSG("Setting Up Fragment Shader");
-
-	fsid = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fsid, 1, (const GLchar**)&fs_src, NULL);
-	glCompileShader(fsid);
-
-	// Check is Shader Compiled
-	glGetShaderiv(fsid, GL_COMPILE_STATUS, &isCompiled);
-
-	if (isCompiled == GL_TRUE) {
-		DEBUG_MSG("Fragment Shader Compiled");
-		isCompiled = GL_FALSE;
-	}
-	else
-	{
-		DEBUG_MSG("ERROR: Fragment Shader Compilation Error");
-	}
-
-	DEBUG_MSG("Setting Up and Linking Shader");
-	progID = glCreateProgram();
-	glAttachShader(progID, vsid);
-	glAttachShader(progID, fsid);
-	glLinkProgram(progID);
-
-	// Check is Shader Linked
-	glGetProgramiv(progID, GL_LINK_STATUS, &isLinked);
-
-	if (isLinked == 1) {
-		DEBUG_MSG("Shader Linked");
-	}
-	else
-	{
-		DEBUG_MSG("ERROR: Shader Link Error");
-	}
-
+	/*CREATE SHADER PROGRAMS*/
+	playerProgID = createProg("vertexShader.txt", "playerFragShader.txt");
+	enemyProgID = createProg("vertexShader.txt", "enemyFragShader.txt");
+	/*END OF SHADER PROGRAM CREATIONS*/
 	// Set image data
 	// https://github.com/nothings/stb/blob/master/stb_image.h
 	img_data = stbi_load(filename.c_str(), &width, &height, &comp_count, 4);
@@ -304,24 +217,14 @@ void Game::initialize()
 	player = mat4(
 		1.0f					// Identity Matrix
 		);
-	enemy[1] = mat4(
-		1.0f					// Identity Matrix
-	);
-	enemy[2] = mat4(
-		1.0f					// Identity Matrix
-	);
-	enemy[3] = mat4(
-		1.0f					//Indentity Matrix
-	);
-	enemy[4] = mat4(
-		1.0f					//Indentity Matrix
-	);
-	enemy[5] = mat4(
-		1.0f					//Indentity Matrix
-	);
-	enemy[6] = mat4(
-		1.0f					//Indentity Matrix
-	);
+
+	for (size_t i = 0; i < 6; i++)
+	{
+		enemy[i] = mat4(
+			1.0f					// Identity Matrix
+		);
+	}
+	
 	barrier1 = mat4(
 		1.0f					//Indentity Matrix
 	);
@@ -334,12 +237,12 @@ void Game::initialize()
 	rotation2 = mat4(
 		1.0f					//Indentity Matrix
 	);
-	enemy[1] = glm::translate(enemy[1], glm::vec3(-3, 0, 0));//LEFT
-	enemy[2] = glm::translate(enemy[2], glm::vec3(3, 0, 0));//RIGHT
-	enemy[3] = glm::translate(enemy[3], glm::vec3(-3, 0, 0));//LEFT
-	enemy[4] = glm::translate(enemy[4], glm::vec3(3, 0, 0));//RIGHT
-	enemy[5] = glm::translate(enemy[5], glm::vec3(-3, 0, 0));//LEFT
-	enemy[6] = glm::translate(enemy[6], glm::vec3(2.8, 0, 0));//MIDDLE
+	enemy[1] = glm::translate(enemy[1], glm::vec3(-3, 0, -250));//LEFT
+	enemy[2] = glm::translate(enemy[2], glm::vec3(3, 0, -250));//RIGHT
+	enemy[3] = glm::translate(enemy[3], glm::vec3(-3, 0, -250));//LEFT
+	enemy[4] = glm::translate(enemy[4], glm::vec3(3, 0, -250));//RIGHT
+	enemy[5] = glm::translate(enemy[5], glm::vec3(-3, 0, -250));//LEFT
+	enemy[0] = glm::translate(enemy[0], glm::vec3(0, 0, -250));//MIDDLE
 
 	rotation1 = glm::translate(rotation1, glm::vec3(-5, 3, 0));//LEFT CORNER
 	rotation2 = glm::translate(rotation2, glm::vec3(5, 3, 0));//RIGHT CORNER
@@ -349,17 +252,100 @@ void Game::initialize()
 	barrier2 = glm::translate(barrier2, glm::vec3(5, 0, 0));
 	barrier2 = glm::scale(barrier2, glm::vec3(0, 1, 100)); //RIGHT BARRIER
 
-
+	player = glm::scale(player, glm::vec3(0.5, 0.5, 0.5));
 	
 	// Enable Depth Test
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LESS);
 	glEnable(GL_CULL_FACE);
-
+	speedMod = 0;
 	// Load Font
 	font.loadFromFile(".//Assets//Fonts//BBrick.ttf");
+	score = 0;
 }
+/*----------------------------------------------CREATE PROG----------------------------------------*/
+GLuint Game::createProg(std::string vertexShader, std::string fragmentShader)
+{
+	GLuint prog = -1;
+	GLint isCompiled = 0;
+	GLint isLinked = 0;
+	// NOTE: uniforms values must be used within Shader so that they 
+	// can be retreived
+	ifstream vertexFile;
+	std::string vString;
+	std::stringstream vContent;
+	vertexFile.open(vertexShader);
+	vContent << vertexFile.rdbuf();
+	vertexFile.close();
+	vString = vContent.str();
 
+	const char* vs_src = vString.c_str();
+
+
+	DEBUG_MSG("Setting Up Vertex Shader");
+
+	vsid = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vsid, 1, (const GLchar**)&vs_src, NULL);
+	glCompileShader(vsid);
+
+	// Check is Shader Compiled
+	glGetShaderiv(vsid, GL_COMPILE_STATUS, &isCompiled);
+
+	if (isCompiled == GL_TRUE) {
+		DEBUG_MSG("Vertex Shader Compiled");
+		isCompiled = GL_FALSE;
+	}
+	else
+	{
+		DEBUG_MSG("ERROR: Vertex Shader Compilation Error");
+	}
+
+	ifstream fragFile;
+	std::string fString;
+	std::stringstream fContent;
+	fragFile.open(fragmentShader);
+	fContent << fragFile.rdbuf();
+	fragFile.close();
+	fString = fContent.str();
+	const char* fs_src = fString.c_str();
+
+	DEBUG_MSG("Setting Up Fragment Shader");
+
+	fsid = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fsid, 1, (const GLchar**)&fs_src, NULL);
+	glCompileShader(fsid);
+
+	// Check is Shader Compiled
+	glGetShaderiv(fsid, GL_COMPILE_STATUS, &isCompiled);
+
+	if (isCompiled == GL_TRUE) {
+		DEBUG_MSG("Fragment Shader Compiled");
+		isCompiled = GL_FALSE;
+	}
+	else
+	{
+		DEBUG_MSG("ERROR: Fragment Shader Compilation Error");
+	}
+
+	DEBUG_MSG("Setting Up and Linking Shader");
+	prog = glCreateProgram();
+	glAttachShader(prog, vsid);
+	glAttachShader(prog, fsid);
+	glLinkProgram(prog);
+
+	// Check is Shader Linked
+	glGetProgramiv(prog, GL_LINK_STATUS, &isLinked);
+
+	if (isLinked == 1) {
+		DEBUG_MSG("Shader Linked");
+	}
+	else
+	{
+		DEBUG_MSG("ERROR: Shader Link Error");
+	}
+
+	return prog;
+}
 void Game::update()
 {
 #if (DEBUG >= 2)
@@ -373,85 +359,42 @@ void Game::update()
 	rotation1 = glm::rotate(rotation1, 0.002f, glm::vec3(0, 0, 1));
 	rotation2 = glm::rotate(rotation2, 0.002f, glm::vec3(0, 0, 1));
 
-	//for (size_t i = 0; i < 6; i++)
-	//{
-	//	enemy[i] = glm::translate(enemy[i], vec3(0, 0, enemyspeed));
-	//	if (enemy[i][3].z > 15)
-	//	{
-	//		enemy[i] = glm::translate(enemy[i], vec3(0, 0, -100));
-	//	}
-	//	//enemyspeed = enemyspeed + 0.000000000032f;
-	//}
-
-	enemy[1] = glm::translate(enemy[1], vec3(0, 0, 0.025));
-	if (enemy[1][3].z > 15)
+	for (size_t i = 0; i < 6; i++)
 	{
-		enemy[1] = glm::translate(enemy[1], vec3(0, 0, -100));
-		enemyVec[1].z = enemy[1][3].z;
-		enemyVec[1].x = enemy[1][3].x;
-	}
-	enemy[2] = glm::translate(enemy[2], vec3(0, 0, 0.02));
-	if (enemy[2][3].z > 15)
-	{
-		enemy[2] = glm::translate(enemy[2], vec3(0, 0, -100));
-		enemyVec[2].z = enemy[2][3].z;
-		enemyVec[2].x = enemy[2][3].x;
-	}
-	enemy[3] = glm::translate(enemy[3], vec3(0, 0, 0.03));
-	if (enemy[3][3].z > 15)
-	{
-		enemy[3] = glm::translate(enemy[3], vec3(0, 0, -100));
-		enemyVec[3].z = enemy[3][3].z;
-		enemyVec[3].x = enemy[3][3].x;
-	}
-	enemy[4] = glm::translate(enemy[4], vec3(0, 0, 0.035));
-	if (enemy[4][3].z > 15)
-	{
-		enemy[4] = glm::translate(enemy[4], vec3(0, 0, -100));
-		enemyVec[4].z = enemy[4][3].z;
-		enemyVec[4].x = enemy[4][3].x;
-	}
-	enemy[5] = glm::translate(enemy[5], vec3(0, 0, 0.02));
-	if (enemy[5][3].z > 15)
-	{
-		enemy[5] = glm::translate(enemy[5], vec3(0, 0, -100));
-		enemyVec[5].z = enemy[5][3].z;
-		enemyVec[5].x = enemy[5][3].x;
+		if (enemy[i][3].z > 15)
+		{
+			enemy[i] = glm::translate(enemy[i], vec3(0, 0, -100));
+			speedMod += 0.001f;
+			score += 100;
+		}
+		//enemyspeed = enemyspeed + 0.000000000032f;
 	}
 
-	enemy[6] = glm::translate(enemy[6], vec3(0, 0, 0.04));
-	if (enemy[6][3].z > 15)
-	{
-		enemy[6] = glm::translate(enemy[6], vec3(0, 0, -100));
-		enemyVec[6].z = enemy[6][3].z;
-		enemyVec[6].x = enemy[6][3].x;
-	}
+	enemy[1] = glm::translate(enemy[1], vec3(0, 0, 0.025 + speedMod));	
+	enemy[2] = glm::translate(enemy[2], vec3(0, 0, 0.02 + speedMod));
+	/*enemy[3] = glm::translate(enemy[3], vec3(0, 0, 0.03));	
+	enemy[4] = glm::translate(enemy[4], vec3(0, 0, 0.035));	
+	enemy[5] = glm::translate(enemy[5], vec3(0, 0, 0.02));*/
+	enemy[0] = glm::translate(enemy[0], vec3(0, 0, 0.04 + speedMod));
+
+	
 
 
 	//----------------------------------COLLISION--------------------------------------//
-	playerRect.setPosition(player[3].x, player[3].z);
-
+	playerRect = sf::FloatRect(player[3].x, player[3].z, 1, 1);
 	for (size_t i = 0; i < 6; i++)
 	{
-	//	enemyRect.setPosition(enemy[i][3].x, enemy[i][3].z);
-
-		//if (enemy[i][3].x <= player[3].x && enemy[i][3].z <= player[3].z)
-		//{
-			
-			//player[3].x = -1000;
-			//std::cout << std::to_string(enemyRect.getGlobalBounds().)
-		//}
-
-		//enemyRect.getGlobalBounds().intersects(playerRect.getGlobalBounds())
-
-
-			if (enemyVec[3].x == playerVec.x && enemyVec[3].z == playerVec.z)
-			{
-				std::cout << "hit" << std::endl;
-			}
+		sf::FloatRect enemyRect = sf::FloatRect(enemy[i][3].x, enemy[i][3].z, 2, 2);
+		if (playerRect.intersects(enemyRect))
+		{
+			std::cout << "hit" << std::endl;
+			isRunning = false;
+		}
+		else
+		{
+			std::cout << std::endl;
+		}
 	}
-
-
 }
 
 void Game::render()
@@ -471,11 +414,8 @@ void Game::render()
 	int x = Mouse::getPosition(window).x;
 	int y = Mouse::getPosition(window).y;
 
-	string hud = "Heads Up Display ["
-		+ string(toString(x))
-		+ "]["
-		+ string(toString(y))
-		+ "]";
+	string hud = "Score: "
+		+ std::to_string(score);
 
 	Text text(hud, font);
 
@@ -489,17 +429,17 @@ void Game::render()
 
 	window.popGLStates();
 	
-	drawCube(player);
+	drawCube(player, playerProgID);
 
 	for (size_t i = 0; i < 6; i++)
 	{
-		drawCube(enemy[i]);
+		drawCube(enemy[i], enemyProgID);
 	}
 
-	drawCube(barrier1);
-	drawCube(barrier2);
-	drawCube(rotation1);
-	drawCube(rotation2);
+	drawCube(barrier1, enemyProgID);
+	drawCube(barrier2, enemyProgID);
+	drawCube(rotation1, enemyProgID);
+	drawCube(rotation2, enemyProgID);
 
 	window.display();
 
@@ -521,7 +461,7 @@ void Game::render()
 		DEBUG_MSG(error);
 	}
 }
-void Game::drawCube(glm::mat4 model)
+void Game::drawCube(glm::mat4 model, GLuint prog)
 {
 	mvp = projection * view * model;
 	// Rebind Buffers and then set SubData
@@ -529,32 +469,32 @@ void Game::drawCube(glm::mat4 model)
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vib);
 
 	// Use Progam on GPU
-	glUseProgram(progID);
+	glUseProgram(prog);
 
 	// Find variables within the shader
 	// https://www.khronos.org/opengles/sdk/docs/man/xhtml/glGetAttribLocation.xml
-	positionID = glGetAttribLocation(progID, "sv_position");
+	positionID = glGetAttribLocation(prog, "sv_position");
 	if (positionID < 0) { DEBUG_MSG("positionID not found"); }
 
-	colorID = glGetAttribLocation(progID, "sv_color");
+	colorID = glGetAttribLocation(prog, "sv_color");
 	if (colorID < 0) { DEBUG_MSG("colorID not found"); }
 
-	uvID = glGetAttribLocation(progID, "sv_uv");
+	uvID = glGetAttribLocation(prog, "sv_uv");
 	if (uvID < 0) { DEBUG_MSG("uvID not found"); }
 
-	textureID = glGetUniformLocation(progID, "f_texture");
-	if (textureID < 0) { DEBUG_MSG("textureID not found"); }
+	textureID = glGetUniformLocation(prog, "f_texture");
+	//if (textureID < 0) { DEBUG_MSG("textureID not found"); }
 
-	mvpID = glGetUniformLocation(progID, "sv_mvp");
+	mvpID = glGetUniformLocation(prog, "sv_mvp");
 	if (mvpID < 0) { DEBUG_MSG("mvpID not found"); }
 
-	x_offsetID = glGetUniformLocation(progID, "sv_x_offset");
+	x_offsetID = glGetUniformLocation(prog, "sv_x_offset");
 	if (x_offsetID < 0) { DEBUG_MSG("x_offsetID not found"); }
 
-	y_offsetID = glGetUniformLocation(progID, "sv_y_offset");
+	y_offsetID = glGetUniformLocation(prog, "sv_y_offset");
 	if (y_offsetID < 0) { DEBUG_MSG("y_offsetID not found"); }
 
-	z_offsetID = glGetUniformLocation(progID, "sv_z_offset");
+	z_offsetID = glGetUniformLocation(prog, "sv_z_offset");
 	if (z_offsetID < 0) { DEBUG_MSG("z_offsetID not found"); };
 
 	// VBO Data....vertices, colors and UV's appended
@@ -589,18 +529,18 @@ void Game::drawCube(glm::mat4 model)
 	// Draw Element Arrays
 	glDrawElements(GL_TRIANGLES, 3 * INDICES, GL_UNSIGNED_INT, NULL);
 }
-void Game::unload()
+void Game::unload(GLuint prog)
 {
 #if (DEBUG >= 2)
 	DEBUG_MSG("Cleaning up...");
 #endif
-	glDetachShader(progID, vsid);	// Shader could be used with more than one progID
-	glDetachShader(progID, fsid);	// ..
+	glDetachShader(prog, vsid);	// Shader could be used with more than one progID
+	glDetachShader(prog, fsid);	// ..
 	glDeleteShader(vsid);			// Delete Vertex Shader
 	glDeleteShader(fsid);			// Delete Fragment Shader
-	glDeleteProgram(progID);		// Delete Shader
+	glDeleteProgram(prog);		// Delete Shader
 	glDeleteBuffers(1, &vbo);		// Delete Vertex Buffer
 	glDeleteBuffers(1, &vib);		// Delete Vertex Index Buffer
-	stbi_image_free(img_data);		// Free image stbi_image_free(..)
+	//stbi_image_free(img_data);		// Free image stbi_image_free(..)
 }
 
--- SFMLOpenGL/Game.h
+++ SFMLOpenGL/Game.h
@@ -17,13 +17,18 @@
 
 #include <Debug.h>
 
+#include <sstream>
+#include <fstream>
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
 using namespace std;
 using namespace sf;
 using namespace glm;
-
+enum class GameState
+{
+	START, GAME_OVER, PLAY
+};
 class Game
 {
 public:
@@ -31,18 +36,28 @@ public:
 	Game(sf::ContextSettings settings);
 	~Game();
 	void run();
+	GLuint createProg(std::string vertexShader, std::string fragmentShader);
 private:
 	RenderWindow window;
 	bool isRunning = false;
 	void initialize();
 	void update();
-	void drawCube(glm::mat4 model);
+	void drawCube(glm::mat4 model, GLuint prog);
 	void render();
-	void unload();
+	void unload(GLuint prog);
+	void unloadPlayer(GLuint prog);
 	mat4 enemy[6];
 	vec3 enemyVec[6], playerVec;
+	//std::vector<vec3> enemyVec;
 	float enemyspeed = 0.02f;
-	sf::RectangleShape enemyRect, playerRect;
+	sf::FloatRect playerRect;
+	float speedMod;
+	float score;
+	GameState currentState;
+	sf::Texture startScreenTexture;
+	sf::Sprite startScreenSprite;
+	sf::Texture endScreenTexture;
+	sf::Sprite endScreenSprite;
 };
 
 #endif
